57deb88aa7f74e68bde0f859c08442f20407e1e7
NIKHIL YADAV <nikhilyadav835402@gmail.com>
2026-03-26 21:25:57 +0530

    first payment

--- first.js
+++ first.js
@@ -2,3 +2,12 @@ const Account_holder="nikhil";
 console.log(Account_holder);
 let bank_balance=748478;
 console.log(bank_balance);
+
+function payment(Deposite){
+    bank_balance+=Deposite;
+    return bank_balance;
+}
+function withdraw(withdraw){
+    bank_balance-=withdraw;
+    return bank_balance;
+}
